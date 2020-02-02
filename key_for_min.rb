1d7367ddbfb70bc16045d79767060eef0a69939b
Claudia Pierson <c172@umbc.edu>
2020-02-02 04:48:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+friends = {:Bisma => 10, :Sean => 27, :Emmanuel => 11}
+key_for_min_value(hash)
 end
